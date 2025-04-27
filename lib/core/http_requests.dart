fd4e145405d75be00bb735979362d9544e2a7051
Koizeay <42680088+Koizeay@users.noreply.github.com>
2025-04-27 20:17:21 +0200

    Set a custom user agent for HTTP requests (GET and POST)

--- lib/core/http_requests.dart
+++ lib/core/http_requests.dart
@@ -1,4 +1,6 @@
 import 'package:http/http.dart' as http;
+import 'dart:io' show Platform;
+import 'package:package_info_plus/package_info_plus.dart';
 
 Future<http.Response> httpGet(String url, Map<String, String> params) async {
   String urlWithParams = url;
@@ -11,9 +13,26 @@ Future<http.Response> httpGet(String url, Map<String, String> params) async {
     });
     urlWithParams = urlWithParams.substring(0, urlWithParams.length - 1);
   }
-  return await http.get(Uri.parse(urlWithParams));
+  Map<String, String> headers = {
+    "User-Agent": await _getToolboxUserAgent()
+  };
+  return await http.get(Uri.parse(urlWithParams), headers: headers);
 }
 
 Future<http.Response> httpPost(String url, Map<String, String> params) async {
-  return await http.post(Uri.parse(url), body: params);
+  Map<String, String> headers = {
+    "User-Agent": await _getToolboxUserAgent()
+  };
+  return await http.post(Uri.parse(url), headers: headers, body: params);
+}
+
+Future<String> _getToolboxUserAgent() async {
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final appName = packageInfo.appName;
+  final appVersion = packageInfo.version;
+  final osName = Platform.operatingSystem;
+  final packageName = packageInfo.packageName;
+  final dartVersion = Platform.version;
+
+  return "$appName/$appVersion ($osName:$packageName) Dart/${dartVersion.split(' ')[0]} (dart:io)";
 }
